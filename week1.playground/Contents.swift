177cd5286e796a9dead561a6a538a942cc0cd6f4
aninaninnannin <aninaninnannin@gmail.com>
2025-09-12 11:24:45 +0800

    week1

---
+++ week1.playground/Contents.swift
@@ -0,0 +1,17 @@
+for i in 1...7 {
+    if i == 1{
+        print("Monday😩")
+    }else if i == 2{
+        print("Tuesday☹️")
+    }else if i == 3{
+        print("Wednesday🙁")
+    }else if i == 4{
+        print("Thursday😕")
+    }else if i == 5{
+        print("Friday🙂")
+    }else if i == 6{
+        print( "Saturday😄")
+    }else if i == 7{
+        print( "Sunday🥹")
+    }
+}
